ffe0593984008a222bae5d5fd84c28614614aa63
Yuma Ohgami <yuma@ohgami.jp>
2017-01-03 20:16:54 +0900

    012_efiversion: add 012_efiversion
    UEFIバージョンの確認サンプル
    
    確認結果
    - QEMU(OVMF) :: 2.4
    - ThinkPad   :: 2.3.1

---
+++ 012_efiversion/BOOTX64.c
@@ -0,0 +1,60 @@
+struct EFI_SYSTEM_TABLE {
+	char _buf1[60];
+	struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
+		void *_buf;
+		unsigned long long (*OutputString)(struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *, unsigned short *);
+	} *ConOut;
+	char _buf2[24];
+	struct {
+		struct {
+			unsigned long long Signature;
+			unsigned int Revision;
+			unsigned int HeaderSize;
+			unsigned int CRC32;
+			unsigned int Reserved;
+		} Hdr;
+	} *BootServices;
+};
+
+unsigned short *int_to_ascii(long long val, unsigned char num_digits, unsigned short str[]);
+
+int DllMainCRTStartup(void *ImageHandle __attribute__ ((unused)), struct EFI_SYSTEM_TABLE *SystemTable)
+{
+	unsigned short str[4];
+
+	SystemTable->ConOut->OutputString(SystemTable->ConOut, L"UEFI version\r\n");
+	SystemTable->ConOut->OutputString(SystemTable->ConOut, L"- Top Half   : ");
+	SystemTable->ConOut->OutputString(SystemTable->ConOut, int_to_ascii(
+						  SystemTable->BootServices->Hdr.Revision >> 16,
+						  5, str));
+	SystemTable->ConOut->OutputString(SystemTable->ConOut, L"\r\n");
+	SystemTable->ConOut->OutputString(SystemTable->ConOut, L"- Bottom Half: ");
+	SystemTable->ConOut->OutputString(SystemTable->ConOut, int_to_ascii(
+						  SystemTable->BootServices->Hdr.Revision & 0x0000ffff,
+						  5, str));
+	SystemTable->ConOut->OutputString(SystemTable->ConOut, L"\r\n");
+
+	while (1);
+
+	return 0;
+}
+
+unsigned short *int_to_ascii(long long val, unsigned char num_digits, unsigned short str[])
+{
+	unsigned char digits_base = 0;
+	char i;
+
+	if (val < 0) {
+		str[digits_base++] = L'-';
+		val *= -1;
+	}
+
+	for (i = num_digits - 1; i >= 0; i--) {
+		str[digits_base + i] = L'0' + (val % 10);
+		val /= 10;
+	}
+
+	str[digits_base + num_digits] = L'\0';
+
+	return str;
+}
