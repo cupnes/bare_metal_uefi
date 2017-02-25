96a0b28a13c573efe0b263a5a1b0c4f9c3194433
Yuma Ohgami <yuma@ohgami.jp>
2017-02-25 23:42:07 +0900

    050_slideshow: generate img.h and img.c
    img.hとimg.cの自動生成機能を追加する。

--- 050_slideshow/img.c
+++
@@ -1,9 +0,0 @@
-#include "img.h"
-
-extern unsigned char _binary_bgra_os5_ss_bgra_start;
-extern unsigned char _binary_bgra_yux_bgra_start;
-
-struct IMG img[NUM_IMGS] = {
-	{640, 356, &_binary_bgra_os5_ss_bgra_start},
-	{640, 480, &_binary_bgra_yux_bgra_start}
-};
